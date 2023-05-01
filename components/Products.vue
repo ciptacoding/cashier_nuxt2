58ed6c3c977867e859c1d9f05956d62ae58df708
ciptacoding <ciptacoding@gmail.com>
2023-05-01 11:50:05 +0700

    feat(products): add new component product

---
+++ components/Products.vue
@@ -0,0 +1,140 @@
+<template>
+  <v-row>
+    <v-col cols="2" v-for="(product, index) in products" :key="index">
+      <v-card :title="product.title" :ripple="true">
+        <v-card-actions>
+          <v-img
+            :src="require(`@/assets/images/products/${product.thumbnail}`)"
+          ></v-img>
+        </v-card-actions>
+        <v-card-text align="center" class="product-title">
+          {{ product.title }}
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          title: 'Asus Zenfone',
+          thumbnail: 'asus-zenfone.png',
+          price: 1500000,
+          categoryId: 1,
+        },
+        {
+          id: 2,
+          title: 'Canon Eos 700D',
+          thumbnail: 'canon-eos-700d.png',
+          price: 2500000,
+          categoryId: 2,
+        },
+        {
+          id: 3,
+          title: 'Canon Eos 750D',
+          thumbnail: 'canon-eos-750d.png',
+          price: 3000000,
+          categoryId: 2,
+        },
+        {
+          id: 4,
+          title: 'Iphone 6 Silver',
+          thumbnail: 'iphone-6-silver.png',
+          price: 4000000,
+          categoryId: 1,
+        },
+        {
+          id: 5,
+          title: 'Lenovo A7000',
+          thumbnail: 'Lenovo-A7000.png',
+          price: 5000000,
+          categoryId: 1,
+        },
+        {
+          id: 6,
+          title: 'LG LED TV 32LF550A',
+          thumbnail: 'lg-32-led-tv-32LF550A.png',
+          price: 6000000,
+          categoryId: 3,
+        },
+        {
+          id: 7,
+          title: 'LG LED TV 32LF520A',
+          thumbnail: 'lg-led-tv32-32LF520A.png',
+          price: 6500000,
+          categoryId: 3,
+        },
+        {
+          id: 8,
+          title: 'Mi 4i',
+          thumbnail: 'mi-4i.png',
+          price: 6500000,
+          categoryId: 1,
+        },
+        {
+          id: 9,
+          title: 'Nikon D3200',
+          thumbnail: 'nikon-d3200.png',
+          price: 4000000,
+          categoryId: 2,
+        },
+        {
+          id: 10,
+          title: 'Nikon D5200',
+          thumbnail: 'nikon-d5200.png',
+          price: 5000000,
+          categoryId: 2,
+        },
+        {
+          id: 11,
+          title: 'Samsung Galaxy A3',
+          thumbnail: 'samsung-galaxy-A3.png',
+          price: 5500000,
+          categoryId: 1,
+        },
+        {
+          id: 12,
+          title: 'Samsung Galaxy A8',
+          thumbnail: 'samsung-galaxy-A8.png',
+          price: 6500000,
+          categoryId: 1,
+        },
+        {
+          id: 13,
+          title: 'Samsung Galaxy Grand Prime',
+          thumbnail: 'samsung-galaxy-grand-prime.png',
+          price: 2500000,
+          categoryId: 1,
+        },
+        {
+          id: 14,
+          title: 'Samsung Galaxy Note 3',
+          thumbnail: 'samsung-galaxy-note-3.png',
+          price: 7500000,
+          categoryId: 1,
+        },
+        {
+          id: 15,
+          title: 'Sharp LED TV 32LE265i',
+          thumbnail: 'sharp-32-led-32LE265i.png',
+          price: 7500000,
+          categoryId: 3,
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.product-title {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>

214c8880c4342536b61e9de8d5a1b51c5bb0930a
ciptacoding <ciptacoding@gmail.com>
2023-05-02 16:17:37 +0700

    feat: add button category

--- components/Products.vue
+++ components/Products.vue
@@ -1,17 +1,38 @@
 <template>
   <v-row>
-    <v-col cols="2" v-for="(product, index) in products" :key="index">
-      <v-card :title="product.title" :ripple="true">
-        <v-card-actions>
-          <v-img
-            :src="require(`@/assets/images/products/${product.thumbnail}`)"
-          ></v-img>
-        </v-card-actions>
-        <v-card-text align="center" class="product-title">
-          {{ product.title }}
-        </v-card-text>
-      </v-card>
-    </v-col>
+    <v-row>
+      <v-col cols="10"> Search </v-col>
+      <v-col cols="2">
+        <v-menu>
+          <template v-slot:activator="{ on: category }">
+            <v-btn v-on="category" color="primary">Category</v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item-group>
+              <v-list-item v-for="(category, index) in categories" :key="index">
+                <v-list-item-title>{{ category.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-menu>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="2" v-for="(product, index) in products" :key="index">
+        <v-card :title="product.title" :ripple="true">
+          <v-card-actions>
+            <v-img
+              :src="require(`@/assets/images/products/${product.thumbnail}`)"
+            ></v-img>
+          </v-card-actions>
+          <v-card-text align="center" class="product-title">
+            {{ product.title }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-row>
 </template>
 
@@ -19,6 +40,11 @@
 export default {
   data() {
     return {
+      categories: [
+        { id: 1, title: 'Smartphone' },
+        { id: 2, title: 'Camera' },
+        { id: 3, title: 'Televisi' },
+      ],
       products: [
         {
           id: 1,

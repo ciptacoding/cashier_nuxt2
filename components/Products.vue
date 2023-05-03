bba40f55413f7f11a06b2196ceace19bc5cb8554
ciptacoding <ciptacoding@gmail.com>
2023-05-03 21:54:21 +0700

    feat(product): create computed filteredProducts for filter category

--- components/Products.vue
+++ components/Products.vue
@@ -9,8 +9,12 @@
           </template>
 
           <v-list>
-            <v-list-item-group>
-              <v-list-item v-for="(category, index) in categories" :key="index">
+            <v-list-item-group v-model="categoryId">
+              <v-list-item
+                v-for="category in categories"
+                :key="category.id"
+                :value="category.id"
+              >
                 <v-list-item-title>{{ category.title }}</v-list-item-title>
               </v-list-item>
             </v-list-item-group>
@@ -20,7 +24,7 @@
     </v-row>
 
     <v-row>
-      <v-col cols="2" v-for="(product, index) in products" :key="index">
+      <v-col cols="2" v-for="(product, index) in filteredProducts" :key="index">
         <v-card :title="product.title" :ripple="true">
           <v-card-actions>
             <v-img
@@ -40,7 +44,9 @@
 export default {
   data() {
     return {
+      categoryId: false,
       categories: [
+        { id: false, title: 'All' },
         { id: 1, title: 'Smartphone' },
         { id: 2, title: 'Camera' },
         { id: 3, title: 'Televisi' },
@@ -151,113 +157,18 @@ export default {
           price: 7500000,
           categoryId: 3,
         },
-        {
-          id: 1,
-          title: 'Asus Zenfone',
-          thumbnail: 'asus-zenfone.png',
-          price: 1500000,
-          categoryId: 1,
-        },
-        {
-          id: 2,
-          title: 'Canon Eos 700D',
-          thumbnail: 'canon-eos-700d.png',
-          price: 2500000,
-          categoryId: 2,
-        },
-        {
-          id: 3,
-          title: 'Canon Eos 750D',
-          thumbnail: 'canon-eos-750d.png',
-          price: 3000000,
-          categoryId: 2,
-        },
-        {
-          id: 4,
-          title: 'Iphone 6 Silver',
-          thumbnail: 'iphone-6-silver.png',
-          price: 4000000,
-          categoryId: 1,
-        },
-        {
-          id: 5,
-          title: 'Lenovo A7000',
-          thumbnail: 'Lenovo-A7000.png',
-          price: 5000000,
-          categoryId: 1,
-        },
-        {
-          id: 6,
-          title: 'LG LED TV 32LF550A',
-          thumbnail: 'lg-32-led-tv-32LF550A.png',
-          price: 6000000,
-          categoryId: 3,
-        },
-        {
-          id: 7,
-          title: 'LG LED TV 32LF520A',
-          thumbnail: 'lg-led-tv32-32LF520A.png',
-          price: 6500000,
-          categoryId: 3,
-        },
-        {
-          id: 8,
-          title: 'Mi 4i',
-          thumbnail: 'mi-4i.png',
-          price: 6500000,
-          categoryId: 1,
-        },
-        {
-          id: 9,
-          title: 'Nikon D3200',
-          thumbnail: 'nikon-d3200.png',
-          price: 4000000,
-          categoryId: 2,
-        },
-        {
-          id: 10,
-          title: 'Nikon D5200',
-          thumbnail: 'nikon-d5200.png',
-          price: 5000000,
-          categoryId: 2,
-        },
-        {
-          id: 11,
-          title: 'Samsung Galaxy A3',
-          thumbnail: 'samsung-galaxy-A3.png',
-          price: 5500000,
-          categoryId: 1,
-        },
-        {
-          id: 12,
-          title: 'Samsung Galaxy A8',
-          thumbnail: 'samsung-galaxy-A8.png',
-          price: 6500000,
-          categoryId: 1,
-        },
-        {
-          id: 13,
-          title: 'Samsung Galaxy Grand Prime',
-          thumbnail: 'samsung-galaxy-grand-prime.png',
-          price: 2500000,
-          categoryId: 1,
-        },
-        {
-          id: 14,
-          title: 'Samsung Galaxy Note 3',
-          thumbnail: 'samsung-galaxy-note-3.png',
-          price: 7500000,
-          categoryId: 1,
-        },
-        {
-          id: 15,
-          title: 'Sharp LED TV 32LE265i',
-          thumbnail: 'sharp-32-led-32LE265i.png',
-          price: 7500000,
-          categoryId: 3,
-        },
       ],
     }
+  },
+  computed: {
+    filteredProducts() {
+      if (this.categoryId) {
+        return this.products.filter(
+          (item) => item.categoryId == this.categoryId
+        )
+      }
+      return this.products
+    },
   },
 }
 </script>
